4c06abe28654b95f0bc25642e57f7985ef0d2353
Andrea <148254879+perezandr@users.noreply.github.com>
2024-01-11 14:32:42 +0100

    Add files via upload

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import AddTodoInput from "./components/addTodoInput";
+import TodoList from "./components/todoList";
+import { list } from "./mocks";
+import "./App.css";
+
+function App() {
+  const [listData, setListData] = useState(list);
+  // const [isVisible, setVisibility] = useState(false);
+
+  // const onHandlePremium = () => setVisibility(true);
+
+  return (
+    <div className="App">
+      {/* Conditional Rendering
+      {!isVisible && (
+        <button onClick={onHandlePremium} style={{ color: "white" }}>
+          Passa alla versione Premium (2000E)
+        </button>
+      // )} */}
+      <h1>TO-DO LIST</h1>
+      <AddTodoInput setListData={setListData} />
+      <hr />
+      <TodoList list={listData} setListData={setListData} />
+    </div>
+  );
+}
+
+export default App;
